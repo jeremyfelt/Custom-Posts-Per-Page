817a7e7662eae2575a43283d8bbbe1b5178fc9d6
Jeremy Felt <jeremy.felt@gmail.com>
2012-02-17 23:00:21 -0800

    Update uninstall, correct comments to reflect updated options.

--- uninstall.php
+++ uninstall.php
@@ -1,9 +1,7 @@
 <?php
-
 /*  Uninstall file for the Custom Posts Per Page plugin. */
-
-/*  The only settings that we add when Custom Posts Per Page is in use are under
- *  the option name 'cpppc_options'. Not much to do for cleanup, but here it is. */
+/*  The only settings added by Custom Posts Per Page are under the option names
+ *  'cpppc_options' and 'cpppc_upgrade'. Not much to do for cleanup, but here it is. */
 
 /*  Check to make sure this file has been called by WordPress and not through any
  *  kind of direct link. */
