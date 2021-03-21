75d942ab606009c94bc3f07198fe3ba401a4d892
jeremyfelt <jeremy.felt@gmail.com>
2021-03-20 17:11:42 -0700

    Clean up uninstall file

--- uninstall.php
+++ uninstall.php
@@ -5,8 +5,9 @@
 
 /*  Check to make sure this file has been called by WordPress and not through any
  *  kind of direct link. */
-if( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) )
+if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
+}
 
 /*  Delete the cpppc_options */
 delete_option( 'cpppc_options' );
